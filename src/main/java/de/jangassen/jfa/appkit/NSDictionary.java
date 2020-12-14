2f960ab2a6024dd73612644ab3e28f8566a7cc6f
Jan Gassen <jan.gassen@vorwerk.de>
2020-12-14 22:02:06 +0100

    Fix dict initialisation

--- src/main/java/de/jangassen/jfa/appkit/NSDictionary.java
+++ src/main/java/de/jangassen/jfa/appkit/NSDictionary.java
@@ -4,7 +4,10 @@ import de.jangassen.jfa.ObjcToJava;
 import de.jangassen.jfa.annotation.NamedArg;
 import de.jangassen.jfa.foundation.ID;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @SuppressWarnings("unused")
 public interface NSDictionary<K, V> extends NSObject {
@@ -15,7 +18,11 @@ public interface NSDictionary<K, V> extends NSObject {
 
   @SuppressWarnings("unchecked")
   static <A, B> NSDictionary<A, B> of(Map<A, B> map) {
-    return ObjcToJava.alloc(NSDictionary.class).initWithObjects(NSArray.of(map.values()), NSArray.of(map.keySet()));
+    List<Map.Entry<A, B>> entries = new ArrayList<>(map.entrySet());
+    List<A> keys = entries.stream().map(Map.Entry::getKey).collect(Collectors.toList());
+    List<B> values = entries.stream().map(Map.Entry::getValue).collect(Collectors.toList());
+
+    return ObjcToJava.alloc(NSDictionary.class).initWithObjects(NSArray.of(values), NSArray.of(keys));
   }
 
   int count();
